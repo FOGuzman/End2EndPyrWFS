fixing the god damn float point calculation

Detach_OOMAO/Pytorch/oomao_functions.py
@@ -143,16 +143,18 @@ def CreateZernikePolynomials(wfs):
     mv = np.array(mf)
     nf  = len(jModes)
     fun = np.zeros((np.size(r),nf))
+    r = np.transpose(r)
+    o = np.transpose(o)
     r = r[pupilLogical]
     o = o[pupilLogical]
-    
+    pupilVec = pupilLogical.flatten()
     
     def R_fun(r,n,m):
         R=np.zeros(np.size(r))
         sran = np.int((n-m)/2)+1
         for s in range(sran):
-            Rn = (-1)**s*np.prod(np.arange(1,(n-s)+1))*r**(n-2*s)
-            Rd = (np.prod(np.arange(1,s+1))*np.prod(np.arange(1,((n+m)/2-s+1)))*np.prod(np.arange(1,((n-m)/2-s)+1)))
+            Rn = (-1)**s*np.prod(np.arange(1,(n-s)+1,dtype=float))*r**(n-2*s)
+            Rd = (np.prod(np.arange(1,s+1))*np.prod(np.arange(1,((n+m)/2-s+1),dtype=float))*np.prod(np.arange(1,((n-m)/2-s)+1)))
             R = R + Rn/Rd
         return(R)    
     

Detach_OOMAO/Pytorch/torch_example.py
@@ -35,10 +35,10 @@ parser = argparse.ArgumentParser(description='Setting, compressive rate, size, a
 parser.add_argument('--modulation', default=1, type=int, help='Pyramid modulation')
 parser.add_argument('--samp', default=2, type=int, help='Sampling')
 parser.add_argument('--D', default=8, type=int, help='Telescope Diameter [m]')
-parser.add_argument('--nPxPup', default=268, type=int, help='Pupil Resolution')
+parser.add_argument('--nPxPup', default=128, type=int, help='Pupil Resolution')
 parser.add_argument('--rooftop', default=[0,0], type=float)
 parser.add_argument('--alpha', default=pi/2, type=float)
-parser.add_argument('--zModes', default=[2,500], type=int, help='Reconstruction Zernikes')
+parser.add_argument('--zModes', default=[2,300], type=int, help='Reconstruction Zernikes')
 parser.add_argument('--PupilConstrain', default=0, type=int, help='Limit information only on pupils of PyrWFS')
 parser.add_argument('--ReadoutNoise', default=0, type=float)
 parser.add_argument('--PhotonNoise', default=0, type=float)
@@ -111,7 +111,7 @@ plt.show(block=False)
 
 #% Control matrix    
     
-phCM = torch.linalg.pinv(wfs.modes)
+phCM = torch.linalg.pinv(torch.tensor(wfs.modes))
 #%% ############# Fourier Phase
 
 from phaseGenerators import *