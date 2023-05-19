1a8b331d31b00e54712f631c7587e30bac51f5a6
FOGuzman <felipe.guzman.v@mail.pucv.cl>
2023-05-18 21:21:26 -0700

    add Fig4Zoomed

--- Detach_OOMAO/Matlab/Figure4B_ComputePlot.m
+++ Detach_OOMAO/Matlab/Figure4B_ComputePlot.m
@@ -4,10 +4,10 @@ clear all;clc
 %% Preconditioners paths
 DPWFS_path = "../Preconditioners/nocap/base/checkpoint/OL1_R128_M0_RMSE0.0285_Epoch_92.mat";
 DPWFSn_path = "../Preconditioners/nocap/pnoise/checkpoint/OL1_R128_M0_RMSE0.05275_Epoch_118.mat";
-savePath = "./ComputeResults/Fig4A/";if ~exist(savePath, 'dir'), mkdir(savePath); end
-matName = "r0PerformanceFig4A";
+savePath = "./ComputeResults/Fig4B/";if ~exist(savePath, 'dir'), mkdir(savePath); end
+matName = "r0PerformanceFig4B";
 FigurePath = "./figures/Figure4/";if ~exist(FigurePath, 'dir'), mkdir(FigurePath); end
-FigureName = "ElementA.pdf";
+FigureName = "ElementB.pdf";
 Compute = true;
 
 
@@ -17,7 +17,7 @@ run("./tools/experiments_settings/F4_settings.m")
 %% Test parameters
 if Compute
 tpr0  = 5000;    % test per r0
-physicalParams.D_R0s = [90 80 70 60 50 40 30 20 10 1];%[30 25 20 15 10 8 5 3 1]
+physicalParams.D_R0s = [30 25 20 15 10 8 5 3 1];%[30 25 20 15 10 8 5 3 1]
 physicalParams.R0s = physicalParams.D./physicalParams.D_R0s;
 rjumps = length(physicalParams.R0s);
 Mods = [0 1 2];
