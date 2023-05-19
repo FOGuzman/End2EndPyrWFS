9586fb9ffeb15191649f0519599b83267b87719f
FOGuzman <felipe.guzman.v@mail.pucv.cl>
2023-05-19 02:58:25 -0400

    comp Fig4

--- Detach_OOMAO/Matlab/Figure4A_ComputePlot.m
+++ Detach_OOMAO/Matlab/Figure4A_ComputePlot.m
@@ -15,7 +15,7 @@ run("./tools/experiments_settings/F4_settings.m")
 
 %% Test parameters
 if Compute
-tpr0  = 5000;    % test per r0
+tpr0  = 1000;    % test per r0
 physicalParams.D_R0s = [90 80 70 60 50 40 30 20 10 1];%[30 25 20 15 10 8 5 3 1]
 physicalParams.R0s = physicalParams.D./physicalParams.D_R0s;
 rjumps = length(physicalParams.R0s);
