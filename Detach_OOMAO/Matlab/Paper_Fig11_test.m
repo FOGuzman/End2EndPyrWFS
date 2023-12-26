bb7587ce5d790653678eb27ef067f63fde507800
FOGuzman <felipe.guzman.v@mail.pucv.cl>
2023-12-26 13:52:21 +0900

    update figs

--- Detach_OOMAO/Matlab/Paper_Fig11_test.m
+++ Detach_OOMAO/Matlab/Paper_Fig11_test.m
@@ -1,10 +1,10 @@
 addpath tools/functions
-clear all;clc;close all
+clear all;clc;%close all
 
-savePath = "./ComputeResults/paper/Fig10/";if ~exist(savePath, 'dir'), mkdir(savePath); end
-matName = "r0PerformanceFig10A";
-matName2 = "r0PerformanceFig8B";
-FigurePath = "./figures/paper/Figure10/";if ~exist(FigurePath, 'dir'), mkdir(FigurePath); end
+savePath = "./ComputeResults/paperRR/Fig11/";if ~exist(savePath, 'dir'), mkdir(savePath); end
+matName = "r0PerformanceFig11A";
+matName2 = "r0PerformanceFig11B";
+FigurePath = "./figures/paper/Figure11/";if ~exist(FigurePath, 'dir'), mkdir(FigurePath); end
 FigureNameA = "fig_P1_rmse.pdf";
 
 %% Test parameters
@@ -16,14 +16,21 @@ r0s = [50 45 40 35 30 25 20 15 10 5 1];
 y1 = R{1}.RMSEpyr(1,:);
 y2 = R{1}.RMSEdpwfs(1,:);
 y3 = R{1}.RMSEdpwfs2(1,:);
-y4 = R{1}.RMSEdpwfs3(1,:);
-y5 = R{1}.RMSEdpwfs4(1,:);
 
-y6  = Rn{1}.RMSEpyr(1,:);
-y7  = Rn{1}.RMSEdpwfs(1,:);
-y8  = Rn{1}.RMSEdpwfs2(1,:);
-y9  = Rn{1}.RMSEdpwfs3(1,:);
-y10 = Rn{1}.RMSEdpwfs4(1,:);
+e1 = R{1}.RMSEpyr(2,:);
+e1 = squeeze(std(R{1}.Alldata(1,:,:),[],2))';
+e2 = R{1}.RMSEdpwfs(2,:);
+e2 = squeeze(std(R{1}.Alldata(2,:,:),[],2))';
+e3 = R{1}.RMSEdpwfs2(2,:);
+e3 = squeeze(std(R{1}.Alldata(3,:,:),[],2))';
+
+y4  = Rn{1}.RMSEpyr(1,:);
+y5  = Rn{1}.RMSEdpwfs(1,:);
+y6  = Rn{1}.RMSEdpwfs2(1,:);
+
+e4  = Rn{1}.RMSEpyr(2,:);
+e5  = Rn{1}.RMSEdpwfs(2,:);
+e6  = Rn{1}.RMSEdpwfs2(2,:);
 
 lbltxt{1} = sprintf("PWFS-M%i, noiseless",R{1}.INFO.modulation);
 lbltxt{2} = sprintf("PWFS-M%i, noisy",R{1}.INFO.modulation);
@@ -32,16 +39,16 @@ lbltxt{4} = sprintf("DPWFS-R1, noisy");
 lbltxt{5} = sprintf("PUPIL-R1, noiseless");
 lbltxt{6} = sprintf("PUPIL-R1, noisy");
 
-fig = figure('Color','w','Units','normalized','Position',[0.5436 0.2204 0.4427 0.4120]);
+fig = figure('Color','w','Units','normalized','Position',[0.5436 0.2204 0.3903 0.4444]);
 firstax = axes (fig, 'FontSize', 16); 
 
-plot(r0s,y1,'-r','LineWidth',1.5)
+errorbar(r0s,y1,e1,'--r','LineWidth',1.5,'CapSize',8)
 hold on
-plot(r0s,y6,'--r','LineWidth',1.5)
-plot(r0s,y2,'-b','LineWidth',1.5)
-plot(r0s,y7,'--b','LineWidth',1.5)
-plot(r0s,y4,'-k','LineWidth',1.5)
-plot(r0s,y9,'--k','LineWidth',1.5)
+errorbar(r0s,y4,e4,':r','LineWidth',1.5,'CapSize',8)
+errorbar(r0s,y2,e2,'--b','LineWidth',1.5,'CapSize',8)
+errorbar(r0s,y5,e5,':b','LineWidth',1.5,'CapSize',8)
+errorbar(r0s,y3,e3,'--k','LineWidth',1.5,'CapSize',8)
+errorbar(r0s,y6,e6,':k','LineWidth',1.5,'CapSize',8)
 
 set(gca,'XDir','reverse','FontSize',16,'XTick',fliplr(r0s),'LineWidth',.8)
 xlabel('$D/r_0$','interpreter','latex','FontSize',16)
@@ -68,3 +75,24 @@ grid on
 exportgraphics(fig,FigurePath+FigureNameA)
 
 
+
+function fig= filled_plot(r0,x,e, color_mean1, transparency)
+
+mean_vec1=x;
+
+error_vec1=e;%/sqrt(length(input_vecs1(:,1)));
+
+color_error1=min(1, color_mean1+0.3);
+
+x1=r0;
+X=[x1,fliplr(x1)];
+y1=mean_vec1+error_vec1/2;
+y2=mean_vec1-error_vec1/2;
+Y=[y1,fliplr(y2)];
+hold on
+fig{1}=fill(X,Y, color_error1, 'EdgeColor', 'none');
+%fig{2}=plot(r0,mean_vec1, 'Color',[color_mean1, 1]);
+set(fig{1},'facealpha',transparency);
+
+
+end
